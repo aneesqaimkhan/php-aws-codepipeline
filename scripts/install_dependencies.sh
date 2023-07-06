fa3f0058dad848cf6d95858214993b7411f02e7a
Ahtisham Saleem <ahtisham.saleem@salesflo.com>
2023-07-06 18:13:36 +0500

    updated

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -23,7 +23,11 @@ yum -y install httpd24
 sed -i 's#AllowOverride None#AllowOverride All#' /etc/httpd/conf/httpd.conf
 
 # Change apache document root
+mkdir -p /var/www/html/public
+sed -i 's#DocumentRoot "/var/www/html"#DocumentRoot "/var/www/html/public"#' /etc/httpd/conf/httpd.conf
 
+# Change apache directory index
+sed -e 's/DirectoryIndex.*/DirectoryIndex index.html index.php/' -i /etc/httpd/conf/httpd.conf
 
 # Get Composer, and install to /usr/local/bin
 if [ ! -f "/usr/local/bin/composer" ]; then
@@ -40,5 +44,14 @@ service httpd start
 # Setup apache to start on boot
 chkconfig httpd on
 
+# Ensure aws-cli is installed and configured
+if [ ! -f "/usr/bin/aws" ]; then
+    curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
+    unzip awscli-bundle.zip
+    ./awscli-bundle/install -b /usr/bin/aws
+fi
 
-
+# Ensure AWS Variables are available
+if [[ -z "$AWS_ACCOUNT_ID" || -z "$AWS_DEFAULT_REGION " ]]; then
+    echo "AWS Variables Not Set.  Either AWS_ACCOUNT_ID or AWS_DEFAULT_REGION"
+fi
