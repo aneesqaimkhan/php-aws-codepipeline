c349cb09383d0ea755f8fbe6deaa9b5552051e64
Ahtisham Saleem <ahtisham.saleem@salesflo.com>
2023-07-06 17:54:59 +0500

    update

--- scripts/install_composer_dependencies.sh
+++ scripts/install_composer_dependencies.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 cd /var/www/html
 yum install wget -y
-wget https://getcomposer.org/composer.phar
-php /var/www/html composer.phar update
+
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php composer-setup.php --install-dir=/usr/bin --filename=composer
+php -r "unlink('composer-setup.php');"
+php /var/www/html composer update
