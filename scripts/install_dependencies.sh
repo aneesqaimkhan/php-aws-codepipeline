61d62a7e2c7d63c41888bb516ca8dc5c3c88431d
Ahtisham Saleem <ahtisham.saleem@salesflo.com>
2023-07-07 11:00:58 +0500

    update

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 if ! [ -x "$(command -v httpd)" ]; then yum install -y httpd24 >&2;   exit 1; fi
 # install apache if not already installed
+
+# Install PHP 7.1
+sudo amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2
+# Install Apache 2.4
+sudo yum install -y httpd mariadb-server
+
+sudo systemctl start httpd
+
+sudo systemctl enable httpd
+
+sudo systemctl is-enabled httpd
