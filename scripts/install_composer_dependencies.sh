59bb0eaea2e890c5ab4aa19b3aefcf43beabff3b
Ahtisham Saleem <ahtisham.saleem@salesflo.com>
2023-07-07 11:06:09 +0500

    Updated

--- scripts/install_composer_dependencies.sh
+++ scripts/install_composer_dependencies.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-cd /var/www/html
 # yum install wget -y
 # wget https://getcomposer.org/composer.phar
 # php /usr/bin/php composer.phar install
+export COMPOSER_ALLOW_SUPERUSER=1
+composer install -d /var/www/html/
